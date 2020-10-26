aff98ec616580bb9a6bc8cafb7c6c8e9bb62e0ca
NandaGopal <ngopal253@gmail.com>
2020-10-26 12:59:08 +0530

    custom player

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -15,14 +15,95 @@ export default function Player({ children, ...restProps }) {
 
 Player.Video = function PlayerVideo({ src, ...restProps }) {
   const { showPlayer, setShowPlayer } = React.useContext(PlayerContext)
+  const videoPlayer = React.createRef()
+  const playButton = React.createRef()
+  const stopButton = React.createRef()
+  const progressBar = React.createRef()
+  const timestamp = React.createRef()
+
+  // Update progress & timestamp
+  const updateProgress = () => {
+    if(showPlayer) {
+      progressBar.current.value = (videoPlayer.current.currentTime / videoPlayer.current.duration) * 100;
+
+      // Get minutes
+      let mins = Math.floor(videoPlayer.current.currentTime / 60);
+      if (mins < 10) {
+        mins = '0' + String(mins);
+      }
+
+      // Get seconds
+      let secs = Math.floor(videoPlayer.current.currentTime % 60);
+      if (secs < 10) {
+        secs = '0' + String(secs);
+      }
+
+      timestamp.current.innerHTML = `${mins}:${secs}`;
+    }
+  }
+
+  // update play/pause icon
+  const updatePlayIcon = () => {
+    if(showPlayer) {
+      if (videoPlayer.current.paused) {
+        playButton.current.innerHTML = '<i class="fa fa-play fa-2x" style="color: white;" />'
+      } else {
+        playButton.current.innerHTML = '<i class="fa fa-pause fa-2x" style="color: white;" />'
+      }
+    }
+  }
+
+
+  // Stop video
+  const stopVideo = () => {
+    if(showPlayer) {
+      videoPlayer.current.currentTime = 0;
+      videoPlayer.current.pause();
+    }
+  }
+
+  // Play & pause video
+  const toggleVideoStatus = () => {
+    if(showPlayer) {
+      if (videoPlayer.current.paused) {
+        videoPlayer.current.play();
+      } else {
+        videoPlayer.current.pause();
+      }
+    }
+  }
+
+  // Set video time to progress
+  const setVideoProgress = () => {
+    if(showPlayer) {
+      videoPlayer.current.currentTime = (+progressBar.current.value * videoPlayer.current.duration) / 100;
+    }
+  }
 
   return showPlayer && ReactDOM.createPortal(
-    <SC.Overlay onClick={() => setShowPlayer(false)} { ...restProps }>
+    <SC.Overlay { ...restProps }>
       <SC.Inner>
-        <video id="netflix-player" controls>
-          <source src={src} type="video/mp4" />
-        </video>
-        <SC.Close />
+        <SC.Video
+         ref={videoPlayer}
+         id="netflix-player"
+         src={src}
+         className='screen'
+         poster='/images/misc/poster.png'
+         onClick={toggleVideoStatus}
+         onPause={updatePlayIcon}
+         onPlay={updatePlayIcon}
+         onTimeUpdate={updateProgress} />
+        <SC.Controls className="controls">
+          <SC.VideoButton ref={playButton} className="btn" id='play' onClick={toggleVideoStatus}>
+            <SC.Icon className="fa fa-play fa-2x"></SC.Icon>
+          </SC.VideoButton>
+          <SC.VideoButton ref={stopButton} className="btn" id='stop' onClick={stopVideo}>
+            <SC.Icon className="fa fa-stop fa-2x"></SC.Icon>
+          </SC.VideoButton>
+          <SC.Progress ref={progressBar} type="range" id='progress' className='progress' min='0' max='100' step='0.1' value='0' onChange={setVideoProgress}/>
+          <SC.Timestamp ref={timestamp} className="timestamp" id="timestamp">00:00</SC.Timestamp>
+        </SC.Controls>
+        <SC.Close onClick={() => setShowPlayer(false)} />
       </SC.Inner>
     </SC.Overlay>,
     document.body
--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -77,3 +77,137 @@ export const Button = styled.button`
     background-color: #ff0a16;
   }
 `
+
+export const Video = styled.video`
+  cursor: pointer;
+  width: 60%;
+  background-color: #000 !important;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`
+
+export const Controls = styled.div`
+  background: #333;
+  color: #fff;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`
+
+export const VideoButton = styled.button`
+  border: 0;
+  background: transparent;
+  cursor: pointer;
+  &:focus {
+    outline: 0;
+  }
+`
+
+export const Icon = styled.i`
+  color: #fff;
+`
+
+export const Progress = styled.input`
+  -webkit-appearance: none;
+  width: 100%;
+  background: transparent;
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-ms-track {
+    width: 100%;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+    margin-top: -14px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  &::-moz-range-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  &::-ms-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  &::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #3071a9;
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  &:focus::-webkit-slider-runnable-track {
+  	background: #367ebd;
+  }
+  &::-moz-range-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #3071a9;
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  &::-ms-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    border-width: 16px 0;
+    color: transparent;
+  }
+  &::-ms-fill-lower {
+    background: #2a6495;
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  &:focus::-ms-fill-lower {
+  	background: #3071a9;
+  }
+  &::-ms-fill-upper {
+    background: #3071a9;
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  &:focus::-ms-fill-upper {
+  	background: #367ebd;
+  }
+`
+
+export const Timestamp = styled.span`
+  color: #fff;
+  font-weight: bold;
+  margin-left: 10px;
+`
